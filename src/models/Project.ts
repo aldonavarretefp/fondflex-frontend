50ca1200d7dc3d51f0ea1b139189c1d58813b72a
Aldo Navarrete <aldo.navarrete@fi.unam.edu>
2024-01-31 22:25:23 +0100

    Added Project prototype model

---
+++ src/models/Project.ts
@@ -0,0 +1,20 @@
+export type Project = {
+    name: string;
+    description: string;
+    type: string;
+    amount: number;
+    amountRaised: number;
+    dateEnd: Date;
+    dateStart: Date;
+    id: string;
+    image: string;
+    interestRate: number;
+    location: string;
+    owner: string;
+    paymentMethod: string; 
+    paymentType: string;
+    risk: string;
+    timeLeft: number;
+    totalInvestors: number;
+    video: string;
+}
