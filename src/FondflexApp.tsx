25fa806421cdc2cbf0fcced10465ca98074041ae
Aldo Navarrete <aldo.navarrete@fi.unam.edu>
2024-01-31 22:26:09 +0100

    Implemented cookies provider

--- src/FondflexApp.tsx
+++ src/FondflexApp.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import AppRouter from './router/AppRouter'
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import AppRouter from "./router/AppRouter";
 
-import { store } from './store';
+import { store } from "./store";
+import { CookiesProvider } from "react-cookie";
 
 const FondFlexApp = () => {
   return (
-
-      <Provider
-        store={store}
-      >
+    <CookiesProvider>
+      <Provider store={store}>
         <BrowserRouter>
-          <AppRouter/>
+          <AppRouter />
         </BrowserRouter>
       </Provider>
-  )
-}
+    </CookiesProvider>
+  );
+};
 
-export default FondFlexApp
+export default FondFlexApp;
