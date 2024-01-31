b1f690fabf22f0e31ac3fa3f5a1a752622cfe384
Aldo Navarrete <aldo.navarrete@fi.unam.edu>
2024-01-31 22:23:23 +0100

    useAuthStoreHook added

--- src/hooks/useAuthStore.ts
+++ src/hooks/useAuthStore.ts
@@ -1,34 +1,53 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { onChecking, onLogout } from "../store/auth/authSlice";
 import fondflexAPI from "../api/fondflexAPI";
+import { UnknownAction } from "@reduxjs/toolkit";
+
 
 export const useAuthStore = () => {
+  
+  const dispatch = useDispatch();
+  
+  const { status, user, errorMessage } = useSelector((state: any) => state.auth);
+  
+  const startLogin = async (email: string, password: string) => {
+    try {
+      const { data } = await fondflexAPI.post("/login", { email, password });
+      console.log(data);
 
-    const dispatch = useDispatch();
-    const state =  useSelector((state: any) => state.auth);
-    const startLogin = async (email: string, password: string) => {
-        try {
-            const { data } = await fondflexAPI.post('/login', { email, password });
-            console.log(data);
-            // dispatch(login(response.data));
-        } catch (error) {
+    } catch (error) {
+      setTimeout(() => {}, 3000);
+    }
+  };
 
-            setTimeout(() => {
-            }, 3000);
-        }
-    };
+  const startLogout = async () => {
+    try {
+      // await axios.post('/logout');
+    } catch (error) {
+      // dispatch(logout());
+    }
+  };
 
-    const startLogout = async () => {
-        try {
-            // await axios.post('/logout');
-
-        } catch (error) {
-            // dispatch(logout());
-        }
-    };
+  const checkAuthToken = async (token: string) => {
+    dispatch(onChecking(null) as UnknownAction);
     
-    return {
-        startLogin,
-        startLogout,
-    };
-}
+    if (token === undefined || !token) {
+      console.error("CheckAuthToken: No token found");
+      dispatch(onLogout(null) as UnknownAction);
+      return;
+    }
+    console.log(`CheckAuthToken: token found ${token}`);
+    try {
+      const response = await fondflexAPI.get("/auth/renewToken");
+      console.log(response);
+      
+    } catch (error) {}
+  };
+
+  return {
+    status,
+    startLogin,
+    startLogout,
+    checkAuthToken,
+  };
+};
